9ec1b75dcdfed0e167da2634284b5d41ce55b5f5
Tiago Fraga <tiagofraga@MBP-de-Tiago.home>
2018-05-12 01:01:58 +0100

    Falta colocar as conclusões dos resultados obtidos

--- TP3/wineAllType.R
+++ TP3/wineAllType.R
@@ -1,9 +1,11 @@
 # Load needed packages
 library(neuralnet)
 library(hydroGOF)
+library(arules)
+library(leaps)
 
 # Read file to memory
-data <- read.csv("~/Desktop/SRCR/Trabalhos/Local/TP3/Dados/winequality-all-type.csv", header=TRUE, sep=";", dec=".")
+data <- read.csv("~/Desktop/SRCR/Trabalhos/Local/TP3/Dados/winequality-all-type.csv", header=TRUE, sep=",", dec=".")
 
 # Mix data
 #data <- data[sample(nrow(data)), ]
@@ -17,110 +19,73 @@ test <- data[5001:6497,]
 
 
 # --------------
-#	Teste A	-> All the atributes	
+#	Teste A	-> Weka atributes	
 # --------------
 
- # Formula to calculate FatigueLevel
+ # Formula to calculate quality of the wines
   formula_A <- quality ~ fixed_acidity + volatile_acidity + citric_acid + residual_sugar + chlorides + free_sulfur_dioxide + total_sulfur_dioxide + density + pH + sulphates + alcohol + type
  
  # See the most important atributes
   selecao_A <- regsubsets(formula_A,data,method="backward")
   summary(selecao_A)
   
-  # Train neural network to predict FatigueLevel
-  rna_A <- neuralnet(formula_A, training, hidden = c(6,4,2), lifesign = "full", linear.output = TRUE, threshold = 0.01)
+  # Formula to calculate quality of the wines with weka's most important atributes
+  formula_Weka <- quality ~ citric_acid + alcohol + type
+  
+  # Train neural network to predict Quality of the wines
+  rna_Weka <- neuralnet(formula_Weka, training, hidden = c(6,4,2), lifesign = "full", linear.output = TRUE, threshold = 0.1)
   
   # Plot neural network
-  plot(rna_A, rep = "best")
+  plot(rna_Weka, rep = "best")
   
   # Define input and test variables
-  test_A <- test
+  test_Weka <- subset(test, select=c("citric_acid" , "alcohol" , "type"))
   
   # Test the neural net with new cases
-  rna_A.results <- compute(rna_A, test_A)
+  rna_Weka.results <- compute(rna_Weka, test_Weka)
   
   # Print the results
-  results_A <- data.frame(current = test$quality, prediction = rna_A.results$net.result)
+  results_Weka <- data.frame(current = test$quality, prediction = rna_Weka.results$net.result)
   
   # Round the results
-  results_A$prediction <- round(results_A$prediction, digits = 0)
+  results_Weka$prediction <- round(results_Weka$prediction, digits = 0)
   
   # Calculate RMSE
-  rmse(c(test$quality), c(results_A$prediction))
+  rmse(c(test$quality), c(results_Weka$prediction))
   
   # Plot RMSE
-  plot(results_A$current, results_A$prediction)
-
+  plot(results_Weka$current, results_Weka$prediction)
+  
+  
 
 
 # --------------
-#	Teste B	-> Without residual sugar and density
+#	Teste B	-> With R atributtes
 # --------------
 
-# Formula to calculate FatigueLevel
-  formula_B <- quality ~ fixed_acidity + volatile_acidity + citric_acid + chlorides + free_sulfur_dioxide + total_sulfur_dioxide + pH + sulphates + alcohol + type
+  # Formula to calculate quality of the wines with weka'a most important atributes
+  formula_R <- quality ~ free_sulfur_dioxide + sulphates + alcohol + type
   
-  # See the most important atributes
-  selecao_B <- regsubsets(formula_B,data,method="backward")
-  summary(selecao_B)
-  
-  # Train neural network to predict FatigueLevel
-  rna_B <- neuralnet(formula_B, training, hidden = c(5,3), lifesign = "full", linear.output = TRUE, threshold = 0.01)
+  # Train neural network to predict Quality
+  rna_R <- neuralnet(formula_R, training, hidden = c(5,3), lifesign = "full", linear.output = TRUE, threshold = 0.01)
   
   # Plot neural network
-  plot(rna_B, rep = "best")
+  plot(rna_R, rep = "best")
   
   # Define input and test variables
-  test_B <- subset(test, select = c("fixed_acidity", "volatile_acidity", "citric_acid", "chlorides","free_sulfur_dioxide","total_sulfur_dioxide","pH","sulphates","alcohol","type"))
+  test_R <- subset(test, select = c("free_sulfur_dioxide" , "sulphates","alcohol" , "type"))
   
   # Test the neural net with new cases
-  rna_B.results <- compute(rna_B, test_B)
+  rna_R.results <- compute(rna_R, test_R)
   
   # Print the results
-  results_B <- data.frame(current = test$quality, prediction = rna_B.results$net.result)
+  results_R <- data.frame(current = test$quality, prediction = rna_R.results$net.result)
   
   # Round the results
-  results_B$prediction <- round(results_B$prediction, digits = 0)
+  results_R$prediction <- round(results_R$prediction, digits = 0)
   
   # Calculate RMSE
-  rmse(c(test$quality), c(results_Bprediction))
+  rmse(c(test$quality), c(results_R$prediction))
   
   # Plot RMSE
-  plot(results_B$current, results_B$prediction)
-
-
-# --------------
-# Teste C -> Without residual sugar, density and chlorides
-# --------------
-
-# Formula to calculate FatigueLevel
-  formula_C <- quality ~ fixed_acidity + volatile_acidity + citric_acid + free_sulfur_dioxide + total_sulfur_dioxide + pH + sulphates + alcohol + type
-  
-  # See the most important atributes
-  selecao_C <- regsubsets(formula_B,data,method="backward")
-  summary(selecao_C)
-  
-  # Train neural network to predict FatigueLevel
-  rna_C <- neuralnet(formula_C, training, hidden = c(3), lifesign = "full", linear.output = TRUE, threshold = 0.01)
-  
-  # Plot neural network
-  plot(rna_C, rep = "best")
-  
-  # Define input and test variables
-  test_C <- subset(test, select = c("fixed_acidity", "volatile_acidity", "citric_acid","free_sulfur_dioxide","total_sulfur_dioxide","pH","sulphates","alcohol"))
-  
-  # Test the neural net with new cases
-  rna_C.results <- compute(rna_C, test_C)
-  
-  # Print the results
-  results_C <- data.frame(current = test$quality, prediction = rna_B.results$net.result)
-  
-  # Round the results
-  results_C$prediction <- round(results_C$prediction, digits = 0)
-  
-  # Calculate RMSE
-  rmse(c(test$quality), c(results_C$prediction))
-  
-  # Plot RMSE
-  plot(results_B$current, results_B$prediction)
-
+  plot(results_R$current, results_R$prediction)
